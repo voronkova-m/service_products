9e9cf64f580e062f63108d01b39f51ea40061d10
Marina <marina.v18@yandex.ru>
2020-10-06 01:49:51 +0300

    fix

--- app.js
+++ app.js
@@ -4,7 +4,11 @@ var config = require('./config/index');
 
 var app = express();
 
-var urlencodedParser = bodyParser.urlencoded({ extended: false});
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: false}));
+
+// parse application/json
+app.use(bodyParser.json());
 
 app.set('view engine', 'ejs');
 app.use('/public', express.static('public'));
@@ -13,21 +17,5 @@ require('./routes')(app);
 
 
 
-/*app.get('/news/:id', function (req, res) {
-    var obj = {title: "Новость", id: 5, paragraphs: ['Параграф', 'Обычный текст', 'Числа 1, 2, 3', 99]};
-    console.log(req.query);
-    res.render('news', {newsID: req.params.id, newParam: 234, obj: obj});
-});
-*/
-
-app.use(function (err, req, res, next) {
-    if (app.get('env') == 'development'){
-        var errorHandler = express.errorHandler();
-        errorHandler(err, req, res, next);
-    }
-    else {
-        res.send(500);
-    }
-});
 
 app.listen(config.get('port'));
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,9 @@
 var Product = require('../models/product').Product;
+var errorhandler = require('errorhandler');
+const request = require('request');
 
-module.exports = function(app) {
+
+module.exports = function (app) {
     app.get('/', function (req, res) {
         res.render('index');
     });
@@ -13,13 +16,110 @@ module.exports = function(app) {
         });
     });
 
-    app.get('/products/:article', function (req, res, next) {
-        Product.find({article: req.params.article}, function (err, product) {
+    app.get('/get_products', function (req, res, next) {
+        Product.find({}, function (err, products) {
             if (err) return next(err);
-            if (!product) {
-                next(new HttpError(404, "Product not found"));
+            res.send(products);
+            return products;
+        });
+    });
+
+    app.get('/products/:id', function (req, res, next) {
+        Product.findById(req.params.id, function (err, product) {
+            if (product == undefined) {
+                res.render('error', {message: "Продукта с таким id нет"});
+            } else {
+                res.render('product', {products: product, typeSearch: "_id", search: req.params.id})
             }
             res.json(product);
+        });
+    });
+
+    app.get('/search', function (req, res) {
+        var article = req.query.article;
+        var type = req.query.type;
+        var name = req.query.name;
+        var trademark = req.query.trademark;
+        request('http://127.0.0.1:3000/get_products', function (err, res2, body) {
+            if (err) {
+                app.use(errorhandler());
+                return;
+            }
+            var products = body;
+            products = JSON.parse(products);
+            var filtrProducts = [];
+            var j = 0;
+            products.forEach(function (product) {
+                if (((product.article === article) || (article === undefined)) &&
+                    ((product.type === type) || (type === undefined)) &&
+                    ((product.name === name) || (name === undefined)) &&
+                    ((product.trademark === trademark) || (trademark === undefined))) {
+                    filtrProducts[j] = product;
+                    j++;
+                }
+            });
+            var typeSearch = [];
+            var search = [];
+            j = 0;
+            if (article != undefined) {
+                typeSearch[j] = 'article';
+                search[j] = article;
+                j++;
+            }
+            if (type != undefined) {
+                typeSearch[j] = 'type';
+                search[j] = type;
+                j++;
+            }
+            if (name != undefined) {
+                typeSearch[j] = 'name';
+                search[j] = name;
+                j++;
+            }
+            if (trademark != undefined) {
+                typeSearch[j] = 'trademark';
+                search[j] = trademark;
+                j++;
+            }
+            //res.render('product', {products: filtrProducts, typesSearch: typeSearch, searches: search});
+            res.send(filtrProducts);
+            return filtrProducts;
+        });
+    });
+
+    app.get("/add_product", function (req, res) {
+        res.render('addProduct');
+    });
+
+    app.post('/addProduct', function (req, res) {
+        var article = req.body.article;
+        var type = req.body.type;
+        var name = req.body.name;
+        var trademark = req.body.trademark;
+        console.log(article);
+        var newProduct = new Product({article: article, type: type, name: name, trademark: trademark});
+        newProduct.save(function (err) {
+            if (err) {
+                res.render('error', {message: err.message});
+                console.log(err.message);
+                return;
+            }
+            var products = body;
+            products = JSON.parse(products);
+            var filtrProducts = [];
+            var j = 0;
+            products.forEach(function (product) {
+                if (((product.article === article) || (article === undefined)) &&
+                    ((product.type === type) || (type === undefined)) &&
+                    ((product.name === name) || (name === undefined)) &&
+                    ((product.trademark === trademark) || (trademark === undefined))) {
+                    filtrProducts[j] = product;
+                    j++;
+                }
+            });
+            //res.render('product', {products: filtrProducts, typesSearch: typeSearch, searches: search});
+            res.send(filtrProducts);
+            return filtrProducts;
         });
     });
 
