9daa6fb2805f55c3bf94bc82ef55e3ebdf205369
Marina <marina.v18@yandex.ru>
2020-11-10 00:49:05 +0300

    fix and add authorization

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express = require('express');
 var bodyParser = require('body-parser');
 var config = require('./config/index');
 
+let passport = require('passport');
 
 var app = express();
 
@@ -10,6 +11,10 @@ app.use(bodyParser.urlencoded({extended: false}));
 
 // parse application/json
 app.use(bodyParser.json());
+
+app.use(passport.initialize());
+require('./middleware/passport')(passport);
+
 
 app.set('view engine', 'ejs');
 app.use('/public', express.static('public'));
